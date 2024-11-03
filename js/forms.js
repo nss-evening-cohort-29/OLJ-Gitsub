f92286051e6c0b613eca1d14d17a9ede5f803b2c
Ozzy Akben <akbenof@gmail.com>
2024-11-02 21:51:55 -0400

    Create pinnedRepos form render

--- js/forms.js
+++ js/forms.js
@@ -5,6 +5,7 @@ import appData from "./data.js";
 import { renderProjects, renderPackages, renderPinnedRepos, renderRepositories } from "./lists.js";
 
 const projectFormSubmit = document.getElementById('createProject');
+const pinnedReposFormSubmit = document.getElementById('pinnedRepoForm');
 
 const newProject = (e) => {
   e.preventDefault();
@@ -18,4 +19,20 @@ const newProject = (e) => {
   projectFormSubmit.reset();
 };
 
+const newPinnedRepo = (e) => {
+  e.preventDefault();
+  const newPinnedRepo = {
+    id: appData.pinnedRepos.length + 1,
+    name: document.querySelector("#repoName").value,
+    description: document.querySelector("#repoDescription").value,
+    language: document.querySelector("#repoLanguage").value,
+    stars: parseInt(document.querySelector("#repoStars").value, 10),
+    forks: parseInt(document.querySelector("#repoForks").value, 10)
+  };
+  appData.pinnedRepos.push(newPinnedRepo);
+  renderPinnedRepos();
+  pinnedReposFormSubmit.reset();
+}
+
+pinnedReposFormSubmit.addEventListener('submit', newPinnedRepo);
 projectFormSubmit.addEventListener('submit', newProject);
